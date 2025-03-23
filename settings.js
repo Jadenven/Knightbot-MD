856028330e793073932bb8155faab5c82ce50830
Jadenven <160222868+Jadenven@users.noreply.github.com>
2025-03-23 15:20:18 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Any',
+  author: 'Jaden',
+  botName: "JustJades",
+  botOwner: 'Jaden', // Your name
+  ownerNumber: '27829212587', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
